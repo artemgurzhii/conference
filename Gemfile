a1bde094d54a741e7dc1d0fd6609f9e198feabaf
Artem Hurzhii <artem.gurzhii@gmail.com>
2016-04-18 01:36:51 +0200

    add first conf block and fixed conf page styles

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 group :jekyll_plugins do
+  gem 'bundler', '~> 1.11', '>= 1.11.2'
   gem 'jekyll-paginate', '~> 1.1'
   gem 'jekyll', '~> 3.1', '>= 3.1.2'
   gem 'redcarpet', '~> 3.3', '>= 3.3.4'
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,48 +1,67 @@
 (function($) {
-	$(function() {
+  $(function() {
 
-		var root = location.protocol + '//' + location.host;
-		$('a').not(':contains(root)').click(function(){
-			this.target = "_blank";
-		});
+    var $main = $('#main');
 
-		var	$window  = $(window),
-				$body	   = $('body'),
-				$menu	   = $('#menu'),
-				$sidebar = $('#sidebar'),
-				$main 	 = $('#main');
+    $main.find('img').addClass('post-page-image');
+    $main.find('h2').removeAttr('id');
 
-		// Disable animations/transitions until the page has loaded.
-		$body.addClass('is-loading');
+    var tags = [
+      "article",
+      "video",
+      "audio",
+      "aside",
+      "footer",
+      "header",
+      "main",
+      "nav",
+      "section",
+      "time",
+    ];
 
-		$window.on('load', () => {
-			$body.removeClass('is-loading');
-		});
+    for(var i = 0, l = tags.length; i < l; i++) {
+      document.createElement(tags[i]);
+    }
 
-		// header search form
-		var $search   = $('.search'),
-		$search_icon  = $search.find('i'),
-		$search_form	= $search.find('form'),
-		$search_input = $search.find('input');
 
-		$body.on('click', '[href="#search"]', (e) => {
-			e.preventDefault();
-			if (!$search_form.hasClass('visible')) {
-				$search_form[0].reset();
-				$search_form.addClass('visible');
-				$search_icon.addClass('');
-				$search_input.focus();
-			}
-		});
+    var  $window  = $(window),
+        $body     = $('body'),
+        $menu     = $('#menu'),
+        $sidebar  = $('#sidebar'),
+        $main     = $('#main');
 
-		$search_input.on('keydown', (e) => {
-			e.preventDefault();
-			if ((e.keyCode === 13 || e.which === 13) && $search_input.val().length === 0)
-				$search_input.blur();
-				return false;
-			})
-			.on('blur', () => {
-				$search_form.removeClass('visible');
-		});
-	});
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass('is-loading');
+
+    $window.on('load', () => {
+      $body.removeClass('is-loading');
+    });
+
+    // header search form
+    var $search   = $('.search'),
+    $search_icon  = $search.find('i'),
+    $search_form  = $search.find('form'),
+    $search_input = $search.find('input');
+
+    $body.on('click', '[href="#search"]', (e) => {
+      e.preventDefault();
+      if (!$search_form.hasClass('visible')) {
+        $search_form[0].reset();
+        $search_form.addClass('visible');
+        $search_icon.addClass('');
+        $search_input.focus();
+      }
+    });
+
+    $search_input.on('keydown', (e) => {
+      e.preventDefault();
+      if ((e.keyCode === 13 || e.which === 13) && $search_input.val().length === 0)
+        $search_input.blur();
+        return false;
+      })
+      .on('blur', () => {
+        $search_form.removeClass('visible');
+    });
+
+  });
 })(jQuery);
