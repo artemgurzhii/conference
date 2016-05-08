c150294f777d83521a6c7beb20d41ac8fc3439b7
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-05-08 18:49:25 +0300

    fixing first tests

---
+++ test/xhr-test.js
@@ -0,0 +1,19 @@
+// mocha --compilers js:babel-core/register
+var xhr = require('../assets/js/modules/xhr.js');
+var chai = require('chai');
+chai.should();
+
+describe('Testing XMLHttpRequest function: ', function() {
+  it('Should get data from *.json file and return value', function () {
+    let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://github.com/artemgurzhii/conference/blob/v.0.1.0/_site/data/search.json', true);
+    xhr.onreadystatechange = function() {
+      console.log("Function returns: " + JSON.stringify(xhr));
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        done();
+      }
+    }
+    xhr.send();
+  });
+});
