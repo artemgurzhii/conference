585baa89f937b9ae430ca62f5be15dc16e4df025
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-05-23 12:48:08 +0200

    other changes

--- test/search-test.js
+++ test/search-test.js
@@ -1,9 +1,16 @@
-// import search from '../assets/js/modules/search';
-import chai   from 'chai';
-let expect = require('chai').expect;
+// Testing if using search input returning new ul > li > a elements which have src set to post link
+// ================
 
-describe('Navbar search conferences by tags/names', function() {
-  it('Should return link nested in <li>', function () {
+// File to import
+// import search from '../assets/js/modules/search';
+
+// Testing framework
+import chai from 'chai';
+import { expect as expect } from 'chai';
+
+// Test
+describe('Navbar search conferences by tags/names', () => {
+  it('Should return link nested in <li>', () => {
     expect(true).to.be.true;
   });
 });
