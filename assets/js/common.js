6699cdbc88e9a6f99be96a510b3071235e47d8fc
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-05-23 12:57:12 +0200

    minor changes

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,6 @@
 require('./libs/geolocator');
 require('./modules/xhr');
-
-if ((window.location.href.indexOf('contact') > -1) || window.location.href === "http://localhost:3000/contact/") {
-  require('./modules/validateEmail');
-}
-
-if (window.location.href === "http://localhost:3000/") {
-  require('./libs/jekyllSearch');
-  require('./libs/searchConfig');
-  require('./modules/search');
-}
+require('./modules/validateEmail');
+require('./libs/jekyllSearch');
+require('./libs/searchConfig');
+require('./modules/search');
