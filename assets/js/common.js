7719fab3d669698efcf04fbe7e2adb756c96b133
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-05-25 16:59:00 +0200

    add page

--- assets/js/common.js
+++
@@ -1,6 +0,0 @@
-require('./libs/geolocator');
-require('./modules/xhr');
-require('./modules/validateEmail');
-require('./libs/jekyllSearch');
-require('./libs/searchConfig');
-require('./modules/search');
