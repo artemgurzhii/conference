5b588d715c818459ed94eddb2d1390fd356a5395
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-06-01 16:10:08 +0200

    Update Jekyll Ruby dependencies. Update posts and add shell script for common commands

--- Gemfile
+++ Gemfile
@@ -5,8 +5,5 @@ group :jekyll_plugins do
   gem 'jekyll-paginate', '~> 1.1'
   gem 'kramdown', '~> 1.10'
   gem 'jekyll', '~> 3.1', '>= 3.1.2'
-  gem 'jekyll-sitemap', '~> 0.10.0'
-  gem 'rake', '~> 11.1', '>= 11.1.2'
-  gem 'rake-jekyll', '~> 1.1'
-  # gem 'jekyll-feed', '~> 0.5.1'
+  gem 'jekyll-feed', '~> 0.5.1'
 end
---
+++ commands.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# GIT
+# Git Clone
+alias gc='git clone '
+# Git Status
+alias gs="git status"
+# Git Commit Message
+alias gcm="git commit -m "$1""
+# Git Add All
+alias gaa="git add ."
+# Git Add Something
+alias gas="git add "$1""
+# Git Push Origin
+alias gpo="git push origin "
+# Gi Log Oneline
+alias glo='git log --oneline'
+# Git Log Colors Graph
+alias glcg="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# NPM
+# Npm Install
+alias ni="npm install "
+# Npm Install --Save
+alias nis="npm i -S "
+# Npm Install --save-Dev
+alias nid="npm i -D "
+# Npm Install Global
+alias nig="npm i -g "
+# Npm Run test
+alias nrt="npm run test"
+# Npm Run
+alias nr='npm run '
+
+# BUILD
+# Gulp Watch
+alias gw='gulp -w'
+# Jekyll Serve
+alias js='jekyll serve'
+# Kill Server
+alias ks="kill $(ps aux | grep '[j]ekyll' | awk '{print $2}')"
