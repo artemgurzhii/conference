08fb18245efc1797445d78d781e3455c618c35d3
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-06-06 20:02:00 +0300

    add jekyll search on paginated pages

--- assets/js/libs/searchConfig.js
+++ assets/js/libs/searchConfig.js
@@ -1,8 +1,15 @@
+let jsonUrl;
+if (window.location.href === 'http://localhost:3000/') {
+  jsonUrl = 'data/search.json';
+} else {
+  jsonUrl = '../../data/search.json';
+}
+
 jekyllSearch({
   input: document.getElementsByClassName('search-input')[0],
   results: document.getElementsByClassName('search-results-list')[0],
   template: '<li><a href="{url}">{title} - {date}</a></li>',
-  json: 'data/search.json',
+  json: jsonUrl,
   noResults: '<li>No results found</li>',
   limit: 10,
   fuzzy: true
