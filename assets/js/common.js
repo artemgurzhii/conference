052ef7672ab2c9424f77a5800fb64177c501b7d6
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-06-08 22:21:52 +0300

    replace github protocol from http: to https:

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,16 @@
+let host = "http://artemgurzhii.github.io/";
+if (window.location.host === host && window.location.protocol !== "https:") {
+  window.location.protocol = "https:";
+}
+
 require('./modules/xhr');
 
-if (window.location.href === "http://localhost:3000/") {
+if (window.location.href === 'http://localhost:3000/' || window.location.href.indexOf('page') > -1) {
   require('./libs/jekyllSearch');
   require('./libs/searchConfig');
   require('./modules/search');
 }
 
-if (window.location.href.indexOf('contact') > -1 || window.location.href === "http://localhost:3000/contact/") {
+if (window.location.href.indexOf('contact') > -1) {
   require('./modules/validateContactForms');
 }
