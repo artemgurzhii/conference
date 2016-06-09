05f1bbcb807d404a8d3aa72fe77cca55862824d6
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-06-09 13:36:29 +0300

    export addClass, hasClass and removeClass functions to use them in other modules

--- assets/js/modules/classes.js
+++ assets/js/modules/classes.js
@@ -1,24 +1,30 @@
-let hasClass = (el, className) => {
+const hasClass = (el, className) => {
   if (el.classList) {
     return el.classList.contains(className);
   } else {
-    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+    return !!el.className.match(new RegExp(`(\\s|^)${className}(\\s|$)`));
   }
 };
 
-let addClass = (el, className) => {
+const addClass = (el, className) => {
   if (el.classList) {
     el.classList.add(className);
   } else if (!hasClass(el, className)) {
-    el.className += " " + className;
+    el.className += ` ${className}`;
   }
 };
 
-let removeClass = (el, className) => {
+const removeClass = (el, className) => {
   if (el.classList) {
     el.classList.remove(className);
   } else if (hasClass(el, className)) {
-    let reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+    const reg = new RegExp(`(\\s|^)${className}(\\s|$)`);
     el.className = el.className.replace(reg, ' ');
   }
+};
+
+module.exports = {
+  addClass: addClass,
+  hasClass: hasClass,
+  removeClass: removeClass
 };
