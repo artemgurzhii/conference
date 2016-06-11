dc183b682eadc6536c6519346c2a8338d2900e5d
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-06-11 15:34:10 +0300

    use in validation form new addEvent function

--- assets/js/modules/validateContactForms.js
+++ assets/js/modules/validateContactForms.js
@@ -1,20 +1,21 @@
-let addClass = require('./classes').addClass;
-let hasClass = require('./classes').hasClass;
-let removeClass = require('./classes').removeClass;
+// imports
+const addClass = require('./classes').addClass;
+const hasClass = require('./classes').hasClass;
+const removeClass = require('./classes').removeClass;
+const addEvent = require('./events').addEvent;
 
 // Main variables
-let
+const
     _form         = document.querySelector('form.feedback-contact'),
     _submitButton = _form.querySelector('.submit-feedback'),
     _name         = _form.querySelector('input[type="text"]'),
     _email        = _form.querySelector('input[type="email"]');
 
-
 // validate form function
 // ================
-let validateForm = (e, filter, inputField, invalidText, validClass, invalidClass) => {
-  let testing = filter.test(inputField.value);  // returning boolean value from testing
-  let _para = document.createElement('p');      // creating paragraph
+const validateForm = (e, filter, inputField, invalidText, validClass, invalidClass) => {
+  const testing = filter.test(inputField.value);  // returning boolean value from testing
+  const _para = document.createElement('p');      // creating paragraph
       _para.textContent = invalidText;          // and adding text content for invalid input value
 
   if (inputField.value.length) {                // if input not empty
@@ -23,38 +24,31 @@ let validateForm = (e, filter, inputField, invalidText, validClass, invalidClass
       if (hasClass(inputField, validClass)) {   // if it has 'valid-input-value' class
         removeClass(inputField, validClass);    // remove it (green border)
       }
-
       if (!hasClass(inputField, invalidClass)) {                           // if it doesn't have 'invalid-input-value' class
         addClass(inputField, invalidClass);                                // add it (red border)
         inputField.parentNode.insertBefore(_para, inputField.nextSibling); // and add notify message
       }
-
     } else {                                                                       // if it DID pass test
-
       if (inputField.parentNode.children.length === 3) {                           // and if it already have notify message
         inputField.parentNode.removeChild(inputField.parentNode.lastElementChild); // remove message
         removeClass(inputField, 'invalid-input-value');                            // and remove 'invalid-input-value' class
       }
-
       addClass(inputField, validClass);                                            // and add 'valid-input-value' class (green border)
-
     }
-
   }
-
 };
 
 // name validation function
-let validateName = () => {
-  let validateNameFilter = /^[a-zA-Z а-яА-Я ]{4,30}$/;
+const validateName = () => {
+  const validateNameFilter = /^[a-zA-Z а-яА-Я ]{4,30}$/;
   validateForm(event, validateNameFilter, _name, 'Invalid name', 'valid-input-value', 'invalid-input-value');
 };
 
 // email validation function
-let validateEmail = () => {
-  let validateEmailFilter = /^(([^<>+()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const validateEmail = () => {
+  const validateEmailFilter = /^(([^<>+()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   validateForm(event, validateEmailFilter, _email, 'Invalid email', 'valid-input-value', 'invalid-input-value');
 };
 
-_submitButton.addEventListener('click', validateName, false);  // adding event on 'name' input submit
-_submitButton.addEventListener('click', validateEmail, false); // adding event on 'email' input submit
+addEvent(_submitButton, 'click', validateName);  // adding event on 'name' input submit
+addEvent(_submitButton, 'click', validateEmail); // adding event on 'email' input submit
