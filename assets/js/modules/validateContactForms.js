f8c758282abb86e94b2b26037276b65496d6d2f6
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-06-20 15:43:53 +0300

    rename search.js to be reusable and update form validation

--- assets/js/modules/validateContactForms.js
+++ assets/js/modules/validateContactForms.js
@@ -5,24 +5,22 @@ const removeClass = require('./classes').removeClass;
 const addEvent = require('./events').addEvent;
 
 // Main variables
-const
-    _form         = document.querySelector('form.feedback-contact'),
-    _submitButton = _form.querySelector('.submit-feedback'),
-    _name         = _form.querySelector('input[type="text"]'),
-    _email        = _form.querySelector('input[type="email"]');
+const _form = document.querySelector('.feedback-contact');
+const _submitBtn = _form.querySelector('.submit-feedback');
+const _name = _form.querySelector('input[type="text"]');
+const _email = _form.querySelector('input[type="email"]');
 
 // validate form function
-// ================
 const validateForm = (e, filter, inputField, invalidText, validClass, invalidClass) => {
-  const testing = filter.test(inputField.value);  // returning boolean value from testing
-  const _para = document.createElement('p');      // creating paragraph
-      _para.textContent = invalidText;          // and adding text content for invalid input value
+  let testing = filter.test(inputField.value);  // returning boolean value from testing
+  const _para = document.createElement('p');    // creating paragraph
+        _para.textContent = invalidText;        // and adding text content for invalid input value
 
-  if (inputField.value.length) {                // if input not empty
-    if (!testing) {                             // and if it DIDN'T pass test
-      e.preventDefault();                       // prevent form from submission
-      if (hasClass(inputField, validClass)) {   // if it has 'valid-input-value' class
-        removeClass(inputField, validClass);    // remove it (green border)
+  if (inputField.value.length) {              // if input not empty
+    if (!testing) {                           // and if it DIDN'T pass test
+      e.preventDefault();                     // prevent form from submission
+      if (hasClass(inputField, validClass)) { // if it has 'valid-input-value' class
+        removeClass(inputField, validClass);  // remove it (green border)
       }
       if (!hasClass(inputField, invalidClass)) {                           // if it doesn't have 'invalid-input-value' class
         addClass(inputField, invalidClass);                                // add it (red border)
@@ -50,5 +48,5 @@ const validateEmail = () => {
   validateForm(event, validateEmailFilter, _email, 'Invalid email', 'valid-input-value', 'invalid-input-value');
 };
 
-addEvent(_submitButton, 'click', validateName);  // adding event on 'name' input submit
-addEvent(_submitButton, 'click', validateEmail); // adding event on 'email' input submit
+addEvent(_submitBtn, 'click', validateName);  // adding event on 'name' input submit
+addEvent(_submitBtn, 'click', validateEmail); // adding event on 'email' input submit
