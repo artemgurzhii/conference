d4958fe256cfce5eaf37131f572098f4e7d87299
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-07-29 22:19:42 +0300

    form validation now works on blur, add minified css

--- assets/js/modules/validateContactForms.js
+++ assets/js/modules/validateContactForms.js
@@ -10,50 +10,52 @@ const _submitBtn = _form.querySelector('.submit-feedback');
 const _name = _form.querySelector('input[type="text"]');
 const _email = _form.querySelector('input[type="email"]');
 
+// Email and name validators
+const validateNameFilter = /^[a-zA-Z а-яА-Я ]{4,30}$/;
+const validateEmailFilter = /^(([^<>+()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 // defining validateForm function
 // this function will addClass Valid if input value passed test, and Invalid class if didn't
-const validateForm = (e, filter, inputField, invalidText, validClass, invalidClass) => {
+const validateForm = (e, filter, inputField, invalidText) => {
   let testing = filter.test(inputField.value);
   const _para = document.createElement('p');
 
-  if (inputField.value.length) {
-    if (!testing) {
-      if (typeof e.preventDefault === 'function') {
-        e.preventDefault();
-      } else {
-        e.returnValue = false;
-      }
-
-      if (hasClass(inputField, validClass)) {
-        removeClass(inputField, validClass);
-      }
-      if (!hasClass(inputField, invalidClass)) {
-        addClass(inputField, invalidClass);
-        _para.textContent = invalidText;
-        inputField.parentNode.insertBefore(_para, inputField.nextElementSibling);
-      }
+  if (!testing) {
+    if (typeof e.preventDefault === 'function') {
+      e.preventDefault();
     } else {
-      if (inputField.parentNode.children.length === 3) {
-        inputField.parentNode.removeChild(inputField.parentNode.lastElementChild);
-        removeClass(inputField, 'invalid-input-value');
-      }
-      addClass(inputField, validClass);
+      e.returnValue = false;
     }
+
+    if (hasClass(inputField, 'valid-input-value')) {
+      removeClass(inputField, 'valid-input-value');
+    }
+    if (!hasClass(inputField, 'invalid-input-value')) {
+      addClass(inputField, 'invalid-input-value');
+      _para.textContent = invalidText;
+      inputField.parentNode.insertBefore(_para, inputField.nextElementSibling);
+    }
+  } else {
+    if (inputField.parentNode.children.length === 3) {
+      inputField.parentNode.removeChild(inputField.parentNode.lastElementChild);
+      removeClass(inputField, 'invalid-input-value');
+    }
+    addClass(inputField, 'valid-input-value');
   }
 };
 
-// name validation function
-const validateName = () => {
-  const validateNameFilter = /^[a-zA-Z а-яА-Я ]{4,30}$/;
+// adding eventlisteners on submit-button click
+addEvent(_submitBtn, 'click', () => {
+  validateForm(event, validateNameFilter, _name, 'Invalid name');
+});
+addEvent(_submitBtn, 'click', () => {
+  validateForm(event, validateEmailFilter, _email, 'Invalid email');
+});
+
+// adding eventlisteners on submit-button blur
+addEvent(_name, 'blur', () => {
   validateForm(event, validateNameFilter, _name, 'Invalid name', 'valid-input-value', 'invalid-input-value');
-};
-
-// email validation function
-const validateEmail = () => {
-  const validateEmailFilter = /^(([^<>+()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+});
+addEvent(_email, 'blur', () => {
   validateForm(event, validateEmailFilter, _email, 'Invalid email', 'valid-input-value', 'invalid-input-value');
-};
-
-// adding eventlisteners on submit-button-click
-addEvent(_submitBtn, 'click', validateName);
-addEvent(_submitBtn, 'click', validateEmail);
+});
