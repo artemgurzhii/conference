9efca7e271e21895d34311990b121cef3f215f6a
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-08-14 13:41:57 +0300

    add return statements to addEvent and removeEvent functions

--- assets/js/modules/events.js
+++ assets/js/modules/events.js
@@ -2,18 +2,18 @@
 // addEvent function
 function addEvent(elem, type, func) {
   if (elem.addEventListener) {
-    elem.addEventListener(type, func, false);
+    return elem.addEventListener(type, func, false);
   } else {
-    elem.attachEvent(`on${type}`, func);
+    return elem.attachEvent(`on${type}`, func);
   }
 }
 
 // defining removeEvent function
 function removeEvent(elem, type, func) {
   if (elem.removeEventListener) {
-    elem.removeEventListener(type, func, false);
+    return elem.removeEventListener(type, func, false);
   } else {
-    elem.detachEvent(`on${type}`, func);
+    return elem.detachEvent(`on${type}`, func);
   }
 }
 
