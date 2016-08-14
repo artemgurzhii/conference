7dc7f1d7f1ecce02790c9c7f94f685fe32267cdf
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-08-14 13:33:41 +0300

    replace for loop with forEach

--- assets/js/modules/all.js
+++ assets/js/modules/all.js
@@ -1,8 +1,21 @@
-const externalLink = document.querySelectorAll('a[href^="http"]');
-let i;
-let linksLength = externalLinks.length;
-for (i = 0; i < linksLength; i++) {
-  if(!externalLinks[i].getAttribute('target')) {
-    externalLinks[i].setAttribute('target', '_blank');
+// all external links will have attribute target='_blank'
+// rel='noopener' so the new page can't access your window object via window.opener
+// and will open in new window
+const links = document.querySelectorAll('a[href^="http"]');
+if (typeof Array.prototype.forEach === 'function') {
+  links.forEach(link => {
+    if(!link.getAttribute('target')) {
+      link.setAttribute('target', '_blank');
+      link.setAttribute('rel', 'noopener');
+    }
+  });
+} else {
+  let i;
+  let linksLength = links.length;
+  for (i = 0; i < linksLength; i++) {
+    if(!links[i].getAttribute('target')) {
+      links[i].setAttribute('target', '_blank');
+      links[i].setAttribute('rel', 'noopener');
+    }
   }
 }
