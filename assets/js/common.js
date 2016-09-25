8736baa8a364c5c2c1fb381cdfa08e60360ea388
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-09-25 11:44:12 +0200

    update todo and replace es5 with es6 method

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,4 @@
-if (window.location.href.indexOf('page') > -1 || window.location.href === 'https://artemgurzhii.github.io/conference/') {
+if (window.location.href.includes('page') || window.location.href === 'https://artemgurzhii.github.io/conference/') {
   require('./libs/jekyllSearch');
   require('./libs/searchConfig');
   require('./modules/removeSearchList');
